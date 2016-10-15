f1a8aebe099d93ed95cc656a2b818312e83482a5
Daniel Steinhauer <d.steinhauer@mailbox.org>
2016-10-15 19:13:52 +0200

    Implemented a more flexible random nr. generator.

--- src/random.cpp
+++ src/random.cpp
@@ -1,16 +1,54 @@
 #include "random.hpp"
 
 #include <fstream>
+#include <time.h>
+#include <stdlib.h>
 
-Random::Random ()
+bool internal_random::StdGenerator::isInitialised = false;
+
+internal_random::RawGenerator::RawGenerator ()
+{}
+
+internal_random::RawGenerator::~RawGenerator ()
+{}
+
+internal_random::Intel_RDRAND_generator::Intel_RDRAND_generator ()
+  : RawGenerator ()
+{}
+
+internal_random::Intel_RDRAND_generator::~Intel_RDRAND_generator ()
+{}
+
+uint64_t internal_random::Intel_RDRAND_generator::getRawNumber ()
+{
+  uint64_t RET;
+  asm (
+    "rdrand %0;"
+    : "=r" (RET)
+  );
+  return RET;
+}
+
+internal_random::UnixGenerator::UnixGenerator ()
+  : RawGenerator ()
 {
   refreshCache ();
 }
 
-Random::~Random()
+internal_random::UnixGenerator::~UnixGenerator ()
 {}
 
-void Random::refreshCache ()
+bool internal_random::UnixGenerator::urandom_exists ()
+{
+  bool RET;
+  std::fstream dev_rand;
+  dev_rand.open ("/dev/urandom", std::ios::in);
+  RET = dev_rand.good ();
+  dev_rand.close ();
+  return RET;
+}
+
+void internal_random::UnixGenerator::refreshCache ()
 {
   std::fstream dev_rand;
   dev_rand.open ("/dev/urandom", std::ios::in);
@@ -20,12 +58,61 @@ void Random::refreshCache ()
   index = 0;
 }
 
-uint64_t Random::getRawNumber()
+uint64_t internal_random::UnixGenerator::getRawNumber ()
 {
   if (index >= 127)  {
     refreshCache ();
   }
   return cache[index++];
+}
+
+internal_random::StdGenerator::StdGenerator ()
+  : RawGenerator ()
+{
+  if (!isInitialised)  {
+    srand (time (NULL));
+    isInitialised = true;
+  }
+}
+
+internal_random::StdGenerator::~StdGenerator ()
+{}
+
+uint64_t internal_random::StdGenerator::getRawNumber()
+{
+  union BigInt
+  {
+    uint64_t Big64;
+    uint32_t Little32[2];
+  } RET;
+  
+  RET.Little32[0] = rand ();
+  RET.Little32[1] = rand ();
+  return RET.Big64;
+}
+
+Random::Random ()
+{
+  uint32_t ecx;
+  asm (
+    "cpuid;"
+    : "=c" (ecx)
+    : "a" (1)
+    : "edx"
+  );
+  
+  if (ecx & (1 << 30))  {
+    generator = new internal_random::Intel_RDRAND_generator ();
+  } else if (internal_random::UnixGenerator::urandom_exists ())  {
+    generator = new internal_random::UnixGenerator ();
+  } else {
+    generator = new internal_random::StdGenerator ();
+  }
+}
+
+Random::~Random()
+{
+  delete generator;
 }
 
 uint64_t Random::getNumber(const uint64_t from, const uint64_t to)
@@ -34,9 +121,9 @@ uint64_t Random::getNumber(const uint64_t from, const uint64_t to)
   if (from == to)  {
     RET = from;
   } else if (from > to)  {
-    RET = (getRawNumber () % (from + 1 - to)) + to;
+    RET = (generator->getRawNumber () % (from + 1 - to)) + to;
   } else  {
-    RET = (getRawNumber () % (to + 1 - from)) + from;
+    RET = (generator->getRawNumber () % (to + 1 - from)) + from;
   }
   return RET;
 }
--- src/random.hpp
+++ src/random.hpp
@@ -3,18 +3,65 @@
 
 #include <stdint.h>
 
+namespace internal_random {
+  class RawGenerator
+  {
+  public:
+    RawGenerator ();
+    virtual ~RawGenerator ();
+    
+    virtual uint64_t getRawNumber () = 0;
+  };
+  
+  class Intel_RDRAND_generator : public RawGenerator
+  {
+  public:
+    Intel_RDRAND_generator ();
+    ~Intel_RDRAND_generator ();
+    
+    uint64_t getRawNumber ();
+  };
+  
+  class UnixGenerator : public RawGenerator
+  {
+  private:
+    uint64_t cache[128];
+    uint8_t index;
+    
+  protected:
+    void refreshCache ();
+    
+  public:
+    UnixGenerator ();
+    ~UnixGenerator ();
+    
+    static bool urandom_exists ();
+    
+    uint64_t getRawNumber ();
+  };
+  
+  class StdGenerator : public RawGenerator
+  {
+  private:
+    static bool isInitialised;
+    
+  public:
+    StdGenerator ();
+    ~StdGenerator ();
+    
+    uint64_t getRawNumber ();
+  };
+}
+
 class Random
 {
 private:
-  uint64_t cache[128];
-  uint8_t index;
+  internal_random::RawGenerator *generator;
   
 public:
   Random ();
   virtual ~Random ();
   
-  void refreshCache ();
-  uint64_t getRawNumber ();
   uint64_t getNumber (const uint64_t, const uint64_t);
 };
 
