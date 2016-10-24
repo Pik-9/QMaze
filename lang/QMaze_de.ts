45dd0ce2ffc3945263213deb4c894e4b5f6f5ca4
Daniel Steinhauer <d.steinhauer@mailbox.org>
2016-10-24 02:57:19 +0200

    Added German translation.

---
+++ lang/QMaze_de.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>QMazeWindow</name>
+    <message>
+        <location filename="../src/QMazeWindow.cpp" line="53"/>
+        <location filename="../src/QMazeWindow.cpp" line="94"/>
+        <source>Finish</source>
+        <translation>Ziel</translation>
+    </message>
+    <message>
+        <location filename="../src/QMazeWindow.cpp" line="54"/>
+        <source>Press [Enter] to get to the next level!</source>
+        <translation>Drücke [Enter], um ins nächste Level zu gelangen!</translation>
+    </message>
+    <message>
+        <location filename="../src/QMazeWindow.cpp" line="90"/>
+        <location filename="../src/QMazeWindow.cpp" line="183"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="../src/QMazeWindow.cpp" line="216"/>
+        <source>Level: %1</source>
+        <translation>Level: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/QMazeWindow.cpp" line="218"/>
+        <source>%1 mazes completed</source>
+        <translation>%1 Labyrinthe absolviert</translation>
+    </message>
+    <message>
+        <location filename="../src/QMazeWindow.cpp" line="225"/>
+        <source>[Esc]: Quit application</source>
+        <translation>[Esc]: Programm beenden</translation>
+    </message>
+    <message>
+        <location filename="../src/QMazeWindow.cpp" line="227"/>
+        <source>[F]: Show maze map</source>
+        <translation>[F]: Zeige ganzes Labyrinth</translation>
+    </message>
+    <message>
+        <location filename="../src/QMazeWindow.cpp" line="229"/>
+        <source>[C]: Show current cell</source>
+        <translation>[C]: Zeige nur aktuelle Zelle</translation>
+    </message>
+</context>
+</TS>
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <QApplication>
+#include <QTranslator>
+#include <QLocale>
 
 #include "QMazeWindow.hpp"
 
 int main (int argc, char *argv[])
 {
   QApplication app (argc, argv);
+  QTranslator trans;
+  QLocale loc;
+  trans.load (QString (":/trans_%1").arg (loc.name ()));
+  app.installTranslator (&trans);
   QMazeWindow *mwin = new QMazeWindow ();
   return app.exec ();
 }
